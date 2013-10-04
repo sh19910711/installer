52f2e7aee38f9e7525ec332eded52e0b9040ac40
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-04 21:21:24 +0900

    Edit spec/command_spec.rb

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'command' do
   it 'm4' do
-    `$HOME/local/bin/m4 --version`.contain?('m4 (GNU M4) 1.4.1') == true
+    ret = `$HOME/local/bin/m4 --version`
+    ret.include?('m4 (GNU M4) 1.4.1') == true
   end
 end
