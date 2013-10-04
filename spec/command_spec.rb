9457d577628c296cea9f7aef52814f7f25baab67
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-04 21:40:07 +0900

    Add spec: make 3.8

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe 'command' do
-  it 'm4' do
+  it 'GNU m4' do
     ret = `$HOME/local/bin/m4 --version`
     ret.include?('m4 (GNU M4) 1.4.1') == true
+  end
+  it 'GNU Make' do
+    ret = `$HOME/local/bin/make --version`
+    ret.include?('GNU Make 3.8') == true
   end
 end
