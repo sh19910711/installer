7b55c0f555b5cfedc8af314b9eff697b7a50566e
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-04 21:42:20 +0900

    Add spec: git 1.8.4

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -3,10 +3,14 @@ require 'spec_helper'
 describe 'command' do
   it 'GNU m4' do
     ret = `$HOME/local/bin/m4 --version`
-    ret.include?('m4 (GNU M4) 1.4.1') == true
+    ret.include?('m4 (GNU M4) 1.4.1').should == true
   end
   it 'GNU Make' do
     ret = `$HOME/local/bin/make --version`
-    ret.include?('GNU Make 3.8') == true
+    ret.include?('GNU Make 3.8').should == true
+  end
+  it 'git' do
+    ret = `$HOME/local/bin/git --version`
+    ret.should('git version 1.8.4').should == true
   end
 end
