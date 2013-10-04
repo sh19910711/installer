4dd9373589b1c419dc54f49eeca3585dfd0c1395
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-04 23:22:48 +0900

    Add spec for tar, coreutils

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -13,4 +13,12 @@ describe 'command' do
     ret = `$HOME/local/bin/git --version`
     ret.include?('git version 1.8.4').should == true
   end
+  it 'tar' do
+    ret = `$HOME/local/bin/tar --version`
+    ret.include?('tar (GNU tar) 1.26').should == true
+  end
+  it 'coreutils' do
+    ret = `$HOME/local/bin/install --version`
+    ret.include?('install (GNU coreutils) 8.21').should == true
+  end
 end
